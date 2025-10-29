462cfa52cb96453b42aa1ac09b28796ee954429a
Mirazul Islam <mirazul3221@gmail.com>
2025-10-30 05:00:53 +0600

    U

--- app/config.js
+++ app/config.js
@@ -3,6 +3,6 @@ export let baseurl01 = 'https://template-engine-u1xl.onrender.com';
 // baseurl01 = 'http://localhost:3300';
 //==========MyPass = @43gnertnr%&Yjk%(&(
 //====================//===============///
-export let viewurl = 'https://bcs-mobile-app-pup6.vercel.app';
+export let viewurl = 'https://mobileeduplusplus.vercel.app';
 // viewurl = 'http://localhost:3000'
 export const PUBLIC_VAPID_PUBLIC_KEY = 'BHXhx_hrq3uah6aFsgz919zUehTyQhlwVElFH14cqIJypU2BE_ndI9CeAh83wo7tYRHMVr5obqmGiukMhrPfO88';
